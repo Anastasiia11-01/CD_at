7f1acdecfd9d7f831baf4af458d9186ba4caa4f1
Anastasiia11-01 <60342160+Anastasiia11-01@users.noreply.github.com>
2023-06-25 16:11:07 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,10 +1,9 @@
 import main
 
-
-
-
 def test_index():
     assert main.index() == 'Hello, world! You are at farm now'
+    response = requests.get("http://157.230.24.236/")
+    assert response.status_code == 200
 
 
 
