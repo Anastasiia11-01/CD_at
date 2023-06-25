3eb1ef0538217c811ff26aca753f2e88aa35634a
Anastasiia11-01 <60342160+Anastasiia11-01@users.noreply.github.com>
2023-06-25 15:06:54 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,17 +5,4 @@ def test_index():
     resp = requests.get(url)
     assert resp.status_code == 200
 
-def test_cow():
-    url = 'http://157.230.24.236/cow'
-    resp = requests.get(url)
-    assert resp.status_code == 200
 
-def test_dog():
-    url = 'http://157.230.24.236/dog'
-    resp = requests.get(url)
-    assert resp.status_code == 200
-
-def test_cat():
-    url = 'http://157.230.24.236/cat'
-    resp = requests.get(url)
-    assert resp.status_code == 200
