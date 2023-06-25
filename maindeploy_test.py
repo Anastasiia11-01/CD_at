2a4f155388b1f3ffcd51ba4bddaee5d76b734cc8
Anastasiia11-01 <60342160+Anastasiia11-01@users.noreply.github.com>
2023-06-25 16:31:11 +0200

    Update maindeploy_test.py

--- maindeploy_test.py
+++ maindeploy_test.py
@@ -1,9 +1,9 @@
 from main import app
 
 def test_index():
-    assert app.index() == 'Hello, world! You are at farm now'
-    """response = requests.get("http://157.230.24.236/")
-    assert response.status_code == 200"""
+    #assert app.index() == 'Hello, world! You are at farm now'
+    response = requests.get("http://157.230.24.236/")
+    assert response.status_code == 200
 
 
 
