10ebbd33bebf79d1d43f66198c2fb716531ab393
Anastasiia Prylutska <anastasiia.prylutska@gmail.com>
2023-06-25 11:43:25 +0200

    First commit

---
+++ test_main.py
@@ -0,0 +1,13 @@
+import main
+
+def test_index():
+    assert main.index() == 'Hello, world! You are at farm now'
+
+def test_cow():
+    assert main.cow() == 'MOoooOo!'
+
+def test_dog():
+    assert main.dog() == 'Wuff! Wuff!'
+
+def test_cat():
+    assert main.cat() == 'MeoooW'
