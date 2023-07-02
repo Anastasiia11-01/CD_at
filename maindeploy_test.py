7f6406081ff21b717aab11993aebd98df1e8e262
Anastasiia11-01 <60342160+Anastasiia11-01@users.noreply.github.com>
2023-07-02 11:01:29 +0200

    Update maindeploy_test.py

--- maindeploy_test.py
+++ maindeploy_test.py
@@ -1,7 +1,7 @@
 import main
 
 def test_index():
-    assert main.index() == 'Assignment done!'
+    assert main.index() == 'Assignment done once again!'
     #response = requests.get("http://157.230.24.236/")
     #assert response.status_code == 200
 
