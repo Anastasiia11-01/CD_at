02de22e83183f6337bacb61746bc163990b66ee2
Anastasiia11-01 <60342160+Anastasiia11-01@users.noreply.github.com>
2023-07-02 11:15:35 +0200

    Update maindeploy_test.py

--- maindeploy_test.py
+++ maindeploy_test.py
@@ -5,6 +5,18 @@ def test_index():
     #response = requests.get("http://157.230.24.236/")
     #assert response.status_code == 200
 
+def test_index():
+    assert main.index() == 'Hello, world! You are at farm now'
+
+def test_cow():
+    assert main.cow() == 'MOoooOo!'
+
+def test_dog():
+    assert main.dog() == 'Wuff! Wuff!'
+
+def test_cat():
+    assert main.cat() == 'MeoooW
+
 
 
 
