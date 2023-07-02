473f2f79e0329453291bd3dbfb2ce1e4c9d0bf32
Anastasiia11-01 <60342160+Anastasiia11-01@users.noreply.github.com>
2023-07-02 10:50:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
-    return 'Hello, world! You are at farm now'
+    return 'Assignment done!'
 
 @app.route('/cow')
 def cow():
