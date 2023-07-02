745c382f21dbbc1a34827a63ddd9659c9282f658
Anastasiia11-01 <60342160+Anastasiia11-01@users.noreply.github.com>
2023-07-02 10:51:05 +0200

    Update maindeploy_test.py

--- maindeploy_test.py
+++ maindeploy_test.py
@@ -1,7 +1,7 @@
 import main
 
 def test_index():
-    assert main.index() == 'Hello, world! You are at farm now'
+    assert main.index() == 'Assignment done!'
     #response = requests.get("http://157.230.24.236/")
     #assert response.status_code == 200
 
